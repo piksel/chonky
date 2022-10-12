0d6c897ce5866c233821fc5e26838e1f35619455
nils måsén <nils.masen@configura.com>
2022-10-12 16:37:28 +0200

    fix button press bugs

--- src/example/ButtonsExample.tsx
+++ src/example/ButtonsExample.tsx
@@ -2,14 +2,14 @@ import { ColorValues, SpecialColors } from "../colors";
 import { Button, ButtonColor, ButtonContextProvider } from "../components";
 
 export const ButtonsExample = () => (<>
-    <div style={{padding: '15px'}}>
+    <div style={{padding: '15px', display: 'flex', flexWrap: 'wrap'}}>
 
         <AllStates><Button primary>Primary</Button></AllStates>
         <AllStates><Button>Not as important</Button></AllStates>
         <AllStates><Button danger>Dangerous!</Button></AllStates>
 
     </div>
-    <div style={{padding: '15px'}}>
+    <div style={{padding: '15px', display: 'flex', flexWrap: 'wrap'}}>
         {ColorValues.filter(c => !SpecialColors.includes(c)).map(c => 
         <AllStates>
             <Button key={c} color={c as ButtonColor}>
@@ -20,7 +20,7 @@ export const ButtonsExample = () => (<>
 </>);
 
 const AllStates: React.FC = ({children}) => (
-    <div style={{display: 'inline-flex', flexDirection: 'column', rowGap: '5px', margin: '5px'}}>
+    <div style={{display: 'flex', minHeight: '80px', justifyContent: 'flex-start', flexDirection: 'column', rowGap: '5px', margin: '5px', marginTop: '0'}}>
     {children}
     <ButtonContextProvider value={{disabled: true}}>
         {children}
